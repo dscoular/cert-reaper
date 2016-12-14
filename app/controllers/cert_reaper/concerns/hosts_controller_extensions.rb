862eb10742053de31c218901fe504e19255ba23c
Doug Scoular <dscoular@cisco.com>
2016-12-14 18:04:43 +1100

    I think I need to use a controller extension so I can call multi host selects as an action.

---
+++ app/controllers/cert_reaper/concerns/hosts_controller_extensions.rb
@@ -0,0 +1,22 @@
+module CertReaper
+  module Concerns
+    module HostsControllerExtensions
+      extend ActiveSupport::Concern
+
+      included do
+        alias_method_chain :multiple_actions, :clear
+      end
+
+      def multiple_actions_with_clear
+        logger.warn("DUG: multiple_actions_with_clear() got called!!!")
+        actions = multiple_actions_without_clear()
+        actions << [_('Clear Puppet Certificate'), multiple_clear_cert]
+        
+        logger.warn('DUG:' + actions.inspect())
+        #      actions <<  [_('Clear Puppet Certificate'), multiple_puppetrun_hosts_path] if Setting[:puppetrun] && authorized_for(:controller => :hosts, :action => :puppetrun)
+        
+      end
+
+    end
+  end
+end
