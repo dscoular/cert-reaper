a4b1698d574f591cbafc502df503aaaa13c0fbe3
Doug Scoular <dscoular@cisco.com>
2016-12-19 08:43:33 +1100

    Taking more advice from rubocop and trying to understand host params.

--- app/controllers/cert_reaper/hosts_controller.rb
+++ app/controllers/cert_reaper/hosts_controller.rb
@@ -1,5 +1,4 @@
 module CertReaper
-
   # Example: Plugin's HostsController inherits from Foreman's HostsController
   extend ProxyAPI
 
@@ -8,23 +7,39 @@ module CertReaper
     # layout 'cert_reaper/layouts/new_layout'
 
     def clear_cert
+      # Since I don't trust the @hosts instance variable to always be
+      # set correctly. I'll assume we are always dealing with params[:id]
+      # and even this seems to sometimes be a hostname string and other times be
+      # a host instance and there's also a params[:host_id] and a params[:id]
+      # in the code.
+      #
       # automatically renders view/cert_reaper/hosts/clear_action
       logger.warn _("DUG: params is: #{params.inspect}.")
       logger.warn _("DUG: class of params[:id] is: #{params[:id].class}.")
-      logger.warn _("DUG: inspect of params[:id] is: #{params[:id].instpect}.")
-      my_host = Host.find_by_name(params[:id])
+      logger.warn _("DUG: inspect of params[:id] is: #{params[:id].inspect}.")
+
+      # I'm pretty confused what the hell host, @host, params[:id], params[:host_id]
+      # and params[:host] all relate to!
+      if params[:id].class != String
+        logger.warn _("DUG: params[:id].class is '#{params[:id].class}' so assigning directly to my_host.")
+        my_host = params[:id]
+      else
+        logger.warn _("DUG: Searching for host in db '#{params[:id]}'.")
+        my_host = Host.find_by(name: params[:id])
+      end
       logger.warn _("DUG: Smart Proxy is #{SmartProxy.inspect}")
-      logger.warn _("DUG: Foreman settings we know are: #{SETTINGS.inspect}");
+      logger.warn _("DUG: Foreman settings we know are: #{SETTINGS.inspect}")
 
       if my_host
-        logger.warn _("DUG: Found host in DB via '#{params[:id]}', my_host is: #{my_host.inspect}.")
+        @my_host = my_host
+        logger.warn _("DUG: @my_host.class is '#{@my_host.class}'.")
         if my_host.try(:certname)
-          logger.warn _("DUG: Successfully found the certificate for this host, you rock!")
+          logger.warn _('DUG: found the certificate for this host, you rock!')
           logger.warn _("DUG: Deleting certificate #{my_host.certname}.")
-          api = ProxyAPI::Puppetca.new({:url => my_host.puppet_ca_proxy.url})
+          logger.warn _("DUG: Using Puppet CA URL #{my_host.puppet_ca_proxy.url}.")
+          api = ProxyAPI::Puppetca.new(:url => my_host.puppet_ca_proxy.url)
           api.del_certificate(my_host.certname)
-          logger.warn _("DUG: Deleted certificate #{my_host.certname}.")
-      
+          logger.warn _("DUG: Deleted certificate #{my_host.certname}.")      
           logger.warn _("DUG: local variables: #{local_variables}")
           logger.warn _("DUG: instance_variables: #{instance_variables}")
           logger.warn _("DUG: global variables: #{global_variables}")
@@ -32,7 +47,7 @@ module CertReaper
           logger.warn _("DUG: No certificate to delete: #{my_host.inspect}.")
         end
       else
-        logger.warn _("DUG: No host found in database!")
+        logger.warn _('DUG: No host found in database!')
       end
       # logger.warn _(HostsController.public_instance_methods)
       # logger.warn _(self.public_methods)
@@ -43,13 +58,13 @@ module CertReaper
     end
 
     private
+
     def multiple_clear_cert
       @hosts.each do |host|
-        logger.warn _("DUG: Deleting certificate #{@host.certname}.")
+        logger.warn _("DUG: Deleting certificate #{host.certname}.")
       end
       notice _('Multiple certificates cleared')
       redirect_back_or_to hosts_path
     end
-
   end
 end
