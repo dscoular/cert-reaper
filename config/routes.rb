7cd238287d5df515fccfb8e8c204da4e4f84e461
Doug Scoular <dscoular@cisco.com>
2016-12-18 21:07:56 -0800

    Added route for multiple_clear_cert.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # DUG: Removed as we don't use this: get 'new_action', to: 'cert_reaper/hosts#new_action'
   get 'clear_cert', to: 'cert_reaper/hosts#clear_cert'
+  get 'multiple_clear_cert', to: 'cert_reaper/hosts#multiple_clear_cert'
 end
