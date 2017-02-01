7c119c48236df4393c0242b683e623fe9480e4ea
Doug Scoular <dscoular@cisco.com>
2017-01-31 17:26:51 -0800

    Removed dead code, tidied up comments and made rubocop happy.

--- app/controllers/cert_reaper/hosts_controller.rb
+++ app/controllers/cert_reaper/hosts_controller.rb
@@ -1,85 +1,83 @@
+# Provides our plugin code for clearing puppet certificates.
 module CertReaper
   ##
   # Our Certificate Reaper's host controller inherits from Foreman's
-  # HostsController.
+  # HostsController. We use the puppet CA proxy's API to do the real
+  # work.
 
   extend ProxyAPI
 
+  ##
+  # This class implements puppet certificate handling for foreman hosts.
   class HostsController < ::HostsController
-    ##
-    # This class implements puppet certificate handling for foreman hosts.
-
+    # Add our two "multiple" actions to the controller's MULTIPLE_ACTIONS array.
     MULTIPLE_ACTIONS << 'multiple_clear_cert'
     MULTIPLE_ACTIONS << 'submit_multiple_clear_cert'
-    logger.warn _("DUG: MULTIPLE ACTIONS = #{MULTIPLE_ACTIONS.inspect}")
+
+    # Load our host resource before every clear_cert action.
     before_action :find_resource, :only => [:clear_cert]
+    # Load our multiple host resources before our "multiple" actions.
     before_action :find_multiple, :only => MULTIPLE_ACTIONS
-#    before_action :find_multiple, :only => [:submit_multiple_clear_cert] #MULTIPLE_ACTIONS
 
-    # def resource_base
-    #   base = super
-    #   base = base.eager_load(:locations) if SETTINGS[:locations_enabled]
-    #   base = base.eager_load(:organizations) if SETTINGS[:organizations_enabled]
-    #   base
-    # end
-
+    ##
+    # Our clear_cert action calls on our common code to clear the current
+    # host's puppet certificate, pops up a notice informing the user of
+    # the attempt to clear the puppet certificate and then redirects back to
+    # the hosts page.
     def clear_cert
-      ##
-      # Our before action on the clear_cert method will have ensured
-      # that our @host instance attribute is either a proper Host object or false. 
+      # Our before action on the clear_cert method will have ensured that
+      # our @host instance attribute is either a proper Host object or false.
       #
       # I'll assume we are always dealing with params[:id] and there
       # also seems to be a params[:host_id] in other parts of the code
-      # which could lead to confusion (at least it does for me.
-      logger.warn _("DUG: params is: #{params.inspect}.")
+      # which could lead to confusion (at least it does for me).
       logger.warn _("DUG: @host instance is: #{@host.inspect}.")
-      logger.warn _("DUG: class of params[:id] is: #{params[:id].class}.")
-      logger.warn _("DUG: inspect of params[:id] is: #{params[:id].inspect}.")
-      logger.warn _("DUG: Smart Proxy is #{SmartProxy.inspect}")
-      logger.warn _("DUG: Foreman settings we know are: #{SETTINGS.inspect}")
       host_clear_cert @host
-      logger.warn _("DUG: local variables: #{local_variables}")
-      logger.warn _("DUG: instance_variables: #{instance_variables}")
-      logger.warn _("DUG: global variables: #{global_variables}")
-      notice _('Cleared certificates for selected host: ' + @host.certname)
+      notice _('Cleared certificates for selected host: ' +
+               @host.try(:certname))
       redirect_to(hosts_path)
-
-      # logger.warn _(HostsController.public_instance_methods)
-      # logger.warn _(self.public_methods)
-      # logger.warn _(self.singleton_methods)
-      # File.open('/tmp/dug.oot', 'w') {
-      #   |file| file.write("clear_cert was called")
-      # }
     end
 
+    ##
+    # Our multiple_clear_cert action seeks confirmation from the user for the
+    # selected hosts.
     def multiple_clear_cert
-      logger.warn _("DUG: INSIDE multiple_clear_cert called. We have params: #{params.inspect}.")
-      logger.warn _("DUG: INSIDE multiple_clear_cert called. We have @hosts: #{@hosts.inspect}.")
-      hosts = @hosts
+      # To be honest I'm not sure how, but this action causes the multi-host
+      # confirmation dialog to be displayed so that the user can confirm their
+      # selection.
     end
 
+    ##
+    # Our submit_multiple_clear_cert action is called after the user has
+    # confirmed the multiple hosts for which they'd like puppet certificates
+    # cleared. Again, we use our common code to clear the certificate for
+    # each host. Once all the host certificates have been cleared we pop up
+    # a notification dialog indicating the hosts involved and then redirect
+    # to the hosts page.
     def submit_multiple_clear_cert
-      logger.warn _("DUG: INSIDE submit_multiple_clear_cert called. We have params: #{params.inspect}.")
-      find_multiple
-      logger.warn _("DUG: INSIDE submit_multiple_clear_cert called. We have @hosts: #{@hosts.inspect}.")
+      # Okay, we've been given an array of hosts as an instance variable.
+      # We'll call our common method to clear the host puppet certificate for
+      # each host..
+      logger.warn _("DUG: @hosts instance is: #{@hosts.inspect}.")
       @hosts.each do |host|
         host_clear_cert host
       end
-      notice _('Cleared certificates for selected hosts: ' + @hosts.map(&:name).join(', '))
+      notice _('Cleared certificates for selected hosts: ' +
+               @hosts.map(&:name).join(', '))
       redirect_to(hosts_path)
     end
 
+    ##
+    # Our action_permission action is called before any action to check that
+    # the user has the appropriate access. To be honest I don't quite understand
+    # how this works or what :edit does.
     def action_permission
-      logger.warn _("DUG: INSIDE action_permission called. We have params: #{params.inspect}.")
       case params[:action]
       when 'clear_cert'
-        logger.warn _("DUG: when clear_cert go to edit.")
         :edit
       when 'multiple_clear_cert'
-        logger.warn _("DUG: when multiple_clear_cert go to edit.")
         :edit
       when 'submit_multiple_clear_cert'
-        logger.warn _("DUG: when submit_multiple_clear_cert go to edit.")
         :edit
       else
         super
@@ -87,48 +85,37 @@ module CertReaper
     end
 
     private
-      def has_CA_feature?(features)
-        features.find { |feature|
-          if feature.try(:name)
-            logger.warn _("DUG: Checking feature name: #{feature.name} against 'Puppet CA'.")
-            logger.warn _("DUG: Inspecting feature: #{feature.inspect}.")
-            feature.name == 'Puppet CA'
-          else
-            logger.warn _("DUG: feature had no name.")
-            false
-          end
-        }
-      end
 
-      def host_clear_cert(host)
-        if host.try(:certname)
-          smart_proxies = SmartProxy.all
-          smart_proxies.each do |smart_proxy|
-            logger.warn _("DUG: smart_proxy class is: #{smart_proxy.class}")
-            logger.warn _("DUG: Inspecting smart_proxy: #{smart_proxy.inspect}")
-            if smart_proxy.try(:features)
-              if has_CA_feature? smart_proxy.features
-                if smart_proxy.try(:url)
-                  api = ProxyAPI::Puppetca.new({:url => smart_proxy.url})
-                  api.del_certificate(host.certname)
-                  logger.warn _("DUG: Deleted certificate #{host.certname}.")
-                  notice _("DUG: Deleted certificate #{host.certname}.")
-                else
-                  logger.warn _("DUG: No url for proxy #{smart_proxy.inspect}.")
-                end
-              else
-                logger.warn _("DUG: No CA feature found in smart_proxy.features: #{smart_proxy.features.inspect}.")
-              end
-            else
-              logger.warn _("DUG: No features found in smart_proxy: #{smart_proxy.inspect}.")
-            end
-          end
-          # api = ProxyAPI::Puppetca.new({:url => host.puppet_ca_proxy.url})
-          # api.del_certificate(host.certname)
+    ##
+    # Our private ca_feature? method determines if our list of features
+    # contains the "Puppet CA" feature.
+    def ca_feature?(features)
+      features.find do |feature|
+        if feature.try(:name)
+          feature.name == 'Puppet CA'
         else
-          logger.warn _("DUG: No certname in host: #{host.inspect}.")
+          false
         end
       end
+    end
 
+    ##
+    # Our host_clear_cert method enumerates all known smart proxies,
+    # We determine if they implement the "Puppet CA" feature and, if they do,
+    # we ask the puppet ProxyAPI to clear the certificate for the cert name.
+    def host_clear_cert(host)
+      return unless host.try(:certname) # Our host doesn't have a certname!
+      # Enumerate all our host's smart proxies.
+      SmartProxy.all do |smart_proxy|
+        # Check if this proxy is relevant i.e. has features, has a CA proxy
+        # feature and an associated proxy URL.
+        next unless smart_proxy.try(:features)
+        next unless ca_feature? smart_proxy.features
+        next unless smart_proxy.try(:url)
+        # This is, indeed, a puppet CA smart proxy we can bend to our will.
+        api = ProxyAPI::Puppetca.new(:url => smart_proxy.url)
+        api.del_certificate(host.certname)
+      end
+    end
   end
 end
