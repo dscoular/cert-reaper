c5f72ac4055300c629f2657ae509ef56c8c42196
Doug Scoular <dscoular@cisco.com>
2017-01-31 17:46:03 -0800

    Tidied up for rubocop.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'clear_cert', to: 'cert_reaper/hosts#clear_cert'
   get 'multiple_clear_cert', to: 'cert_reaper/hosts#multiple_clear_cert'
-  post 'submit_multiple_clear_cert', to: 'cert_reaper/hosts#submit_multiple_clear_cert'end
+  post 'submit_multiple_clear_cert',
+       to: 'cert_reaper/hosts#submit_multiple_clear_cert'
+end
