34de31290871f0674e99680ef0f6b74bf2b4391b
Doug Scoular <dscoular@cisco.com>
2017-02-01 02:48:00 -0800

    Trying to add API calls.

---
+++ app/controllers/api/v2/certs_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module v2
+    class CertsController < ::Api::v2::BaseController
+
+      resource_description do
+        resource_id 'certs'
+        api_version 'v2'
+        api_base_url '/api'
+      end
+
+      api :DELETE, '/certs/:certname/', _('Clear a puppet certificate.')
+      param :certname, :required => true, String, desc: 'Full name of the user'
+
+      def destroy
+        render :json => { :error => _('Destroy got called with "#{params['certname']}".' }, :status => :precondition_failed
+      end
+    end
+  end
+end
