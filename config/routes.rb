db1d72d970f53e66620665de69da0449627bf201
Doug Scoular <dscoular@cisco.com>
2017-02-08 19:16:26 +1100

    Added HTTP DELETE method to cert_reaper/api/(v1,v2)?/certs/:certname API route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,24 @@ Rails.application.routes.draw do
   get 'multiple_clear_cert', to: 'cert_reaper/hosts#multiple_clear_cert'
   post 'submit_multiple_clear_cert',
        to: 'cert_reaper/hosts#submit_multiple_clear_cert'
-  delete 'certs', to: 'cert_reaper/api/v2/certs#destroy'
+  namespace :cert_reaper do
+    namespace :api do
+      scope '(:apiv)', :module => :v2,
+                       :defaults => { :apiv => 'v2' },
+                       :apiv => /v1|v2/,
+                       :constraints => ApiConstraints.new(:version => 2) do
+        # resource :certs, :only => [:destroy]
+        delete '/certs/:certname',
+               controller: 'certs',
+               to: :destroy,
+               # The default constraint doesn't allow "." so we have to
+               # explicitly set the constraint to a regex for fqdn hostnames.
+               constraints: {
+                 certname: /([a-zA-Z0-9](?:(?:[a-zA-Z0-9-]*|
+                           (?<!-)\.(?![-.]))*[a-zA-Z0-9]+)?)/x
+               }
+      end
+    end
+  end
+  #
 end
