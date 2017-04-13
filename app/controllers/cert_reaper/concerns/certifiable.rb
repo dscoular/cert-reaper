811f913e824944507289675a146cd79522296b92
Doug Scoular <dscoular@cisco.com>
2017-04-13 18:38:57 +1000

    Added more logging. Stopped using deprecated "SmartProxy.all" method on the SmartProxy model and used the new "SmartProxy.find_each" method instead.

--- app/controllers/cert_reaper/concerns/certifiable.rb
+++ app/controllers/cert_reaper/concerns/certifiable.rb
@@ -33,16 +33,22 @@ module CertReaper
       # We determine if they implement the "Puppet CA" feature and, if they do,
       # we ask the puppet ProxyAPI to clear the certificate for the cert name.
       def host_clear_cert(host)
+        logger.info _("DUG: host_clear_cert called with [ #{host.inspect}].")
         return unless host.try(:certname) # Our host doesn't have a certname!
+        logger.info _("DUG: host_clear_cert: had a certname.")
         # Enumerate all our host's smart proxies.
-        SmartProxy.all do |smart_proxy|
+        logger.info _("DUG: host_clear_cert: about to enumerate all smart proxies [#{SmartProxy.inspect}].")
+        SmartProxy.find_each do |smart_proxy|
           # Check if this proxy is relevant i.e. has features, has a CA proxy
           # feature and an associated proxy URL.
+          logger.info _("DUG: host_clear_cert: found a smart proxy, checking features for ca_puppet: [#{smart_proxy[:features]}].")
           next unless smart_proxy.try(:features)
           next unless ca_feature? smart_proxy.features
           next unless smart_proxy.try(:url)
           # This is, indeed, a puppet CA smart proxy we can bend to our will.
+          logger.info _("DUG: host_clear_cert: found a puppet CA smart proxy, calling new proxy with url: [#{smart_proxy.url}].")
           api = ProxyAPI::Puppetca.new(:url => smart_proxy.url)
+          logger.info _("DUG: host_clear_cert: calling api.del_certificate with: [#{host.certname}].")
           api.del_certificate(host.certname)
         end
       end
