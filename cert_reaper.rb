96ca0874bad1cc3f19eea55be3a5bb448740b017
Doug Scoular <dscoular@cisco.com>
2017-04-13 18:39:39 +1000

    Better comments and indicate that deface is a dependency.

--- cert_reaper.rb
+++ cert_reaper.rb
@@ -1,2 +1,8 @@
-# Gemfile.local.rb
+# This file should be copied to your foreman installation's bundler.d directory.
+# Our plugin is dependant on the "deface" module.
+gem 'deface', '~> 1.1.0'
+# The :path symbol should point at wherever your cert_reaper source lives if
+# you are planning to develop cert_reaper.
 gem 'cert_reaper', :path => '/root/cert_reaper'
+# Otherwise, you can just refer to the cert_reaper gem version e.g.
+# gem 'cert_reaper', '>= 0.0.9
